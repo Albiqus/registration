53be6109cc7c4c527777a2e17329ed61147caf4f
Альберт <albiqus@bk.ru>
2023-03-06 13:14:54 +0300

    9. Добавлена конфигурация для хостинга на github дополнительно: - Добавлена иконка сайта - Добавлено название сайта

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -4,17 +4,21 @@ import { Header } from "./Header/Header"
 import { Div } from "./Registration-styles"
 import { StepsSlider } from "./StepsSlider/StepsSlider"
 import { SuccessWindow } from "./SuccessWindow/SuccessWindow"
+import { useEffect } from 'react';
+
 
 export const Registration = () => {
 
     const isSubmitData: boolean = useSelector((state: RootState) => state.steps.isSubmitData)
+    useEffect(() => {
+        document.title = "registration"
+    }, []);
 
     return (
         <Div>
             {!isSubmitData && <Header />}
             {!isSubmitData && <StepsSlider />}
             {isSubmitData && <SuccessWindow />}
-
         </Div>
     )
 }
